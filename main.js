2761f0bc1ac3133b8fd4fa3aacee33c69de528f8
lemonhtml <145252154+lemonhtml@users.noreply.github.com>
2023-10-26 21:19:25 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// main.js
+
+// Function to handle the click event of the "Submit" button
+function handleSubmitClick() {
+    alert("Delivered unsuccessful!");
+}
+
+// Add an event listener to the "Submit" button
+document.querySelector("button").addEventListener("click", handleSubmitClick);
